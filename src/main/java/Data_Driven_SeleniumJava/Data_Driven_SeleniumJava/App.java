a40e755a2a28ff751cc554037cbd7833f9936735
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-14 05:24:02 +0530

    Data Driven Framework

---
+++ src/main/java/Data_Driven_SeleniumJava/Data_Driven_SeleniumJava/App.java
@@ -0,0 +1,13 @@
+package Data_Driven_SeleniumJava.Data_Driven_SeleniumJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
