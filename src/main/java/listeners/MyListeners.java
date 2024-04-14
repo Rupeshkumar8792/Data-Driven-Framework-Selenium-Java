1ee0b3ecfa02d949b4bccdb4b9b9c3b60939720d
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-14 09:40:50 +0530

    listener class

--- src/main/java/listeners/MyListeners.java
+++ src/main/java/listeners/MyListeners.java
@@ -1,58 +1,86 @@
 package listeners;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class MyListeners implements ITestListener
-{
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import utils.ExtentReporter;
+import utils.Utilities;
+
+public class MyListeners implements ITestListener {
+	ExtentReports extentReport;
+	ExtentTest extentTest;
+
+	@Override
+	public void onStart(ITestContext context) {
+		
+		extentReport = ExtentReporter.generateExtentReport();
+	
+	}
+
 	@Override
 	public void onTestStart(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestStart(result);
+		
+		extentTest = extentReport.createTest(result.getName());
+		extentTest.log(Status.INFO,result.getName()+" started executing");
+		
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestSuccess(result);
+		
+		extentTest.log(Status.PASS,result.getName()+" got successfully executed");
+		
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestFailure(result);
+		
+		WebDriver driver = null;
+		
+		try {
+			driver = (WebDriver)result.getTestClass().getRealClass().getDeclaredField("driver").get(result.getInstance());
+		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		}
+		
+		String destinationScreenshotPath = Utilities.captureScreenshot(driver,result.getName());
+		
+		extentTest.addScreenCaptureFromPath(destinationScreenshotPath);
+		extentTest.log(Status.INFO,result.getThrowable());
+		extentTest.log(Status.FAIL,result.getName()+" got failed");
+		
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestSkipped(result);
-	}
-
-	@Override
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
-	}
-
-	@Override
-	public void onTestFailedWithTimeout(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestFailedWithTimeout(result);
-	}
-
-	@Override
-	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onStart(context);
+		
+		extentTest.log(Status.INFO,result.getThrowable());
+		extentTest.log(Status.SKIP, result.getName()+" got skipped");
+		
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onFinish(context);
+		
+		extentReport.flush();
+		
+		String pathOfExtentReport = System.getProperty("user.dir")+"\\test-output\\ExtentReports\\extentReport.html";
+		File extentReport = new File(pathOfExtentReport);
+		
+		try {
+			Desktop.getDesktop().browse(extentReport.toURI());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
-	
-	
 }
